Fix the memory leakage problem finally (I guess)

website/label_image.py
@@ -1,51 +1,15 @@
+# Use retrained model to predict uploaded image
+# return the probability matrix and strings back to html
+
 import numpy as np
 import tensorflow as tf
 import cv2
 import os
+from read_tensor_from_image_file import read_tensor_from_image_file
 
-def load_graph(model_file):
-    graph = tf.Graph()
-    graph_def = tf.GraphDef()
-    with open(model_file, "rb") as f:
-        graph_def.ParseFromString(f.read())
-    with graph.as_default():
-        tf.import_graph_def(graph_def)
-    return graph
+def label_image(filename, graph, labels):
 
-def load_labels(label_file):
-    label = []
-    proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
-    for l in proto_as_ascii_lines:
-        label.append(l.rstrip())
-    return label
-
-def read_tensor_from_image_file(file_name,
-                                input_height=299,
-                                input_width=299,
-                                input_mean=0,
-                                input_std=255):
-    input_name = "file_reader"
-    output_name = "normalized"
-    file_reader = tf.read_file(file_name, input_name)
-    if file_name.endswith(".png"):
-        image_reader = tf.image.decode_png(file_reader, channels=3, name="png_reader")
-    elif file_name.endswith(".gif"):
-        image_reader = tf.squeeze(tf.image.decode_gif(file_reader, name="gif_reader"))
-    elif file_name.endswith(".bmp"):
-        image_reader = tf.image.decode_bmp(file_reader, name="bmp_reader")
-    else:
-        image_reader = tf.image.decode_jpeg(file_reader, channels=3, name="jpeg_reader")
-    float_caster = tf.cast(image_reader, tf.float32)
-    dims_expander = tf.expand_dims(float_caster, 0)
-    resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
-    normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-    sess = tf.Session()
-    result = sess.run(normalized)
-    sess.graph.finalize()
-    return result
-
-def label_image(filename):
-
+    # Save a scaled image at static/images/test.jpg for later test
     img = cv2.imread(filename)
     rows = img.shape[0]
     cols = img.shape[1]
@@ -58,6 +22,8 @@ def label_image(filename):
     img = cv2.resize(img,(cols,rows))
     basepath = os.path.dirname(__file__)
     cv2.imwrite(os.path.join(basepath, 'static/images', 'test.jpg'), img)
+
+    # FDA mecury data
     mercury = {'salmon': 'Mercury level (PPM): 0.022.', \
     'trout': 'Mercury level (PPM): 0.071.', \
     'white seabass': 'Mercury level (PPM): 0.354', \
@@ -69,26 +35,26 @@ def label_image(filename):
     'perch': 'Mercury level (PPM): 0.150', \
     'carp': 'Mercury level (PPM): 0.110', \
     'lake whitefish': 'Mercury level (PPM): 0.089', \
-                'shad': 'Mercury level (PPM): 0.038'
+    'shad': 'Mercury level (PPM): 0.038'
     }
     sugg = {'salmon': 'Having 2 servings per week may be OK', \
     'trout': 'Having 2 servings per week may be OK', \
-    'white seabass': 'Not suggested to eat more than 1 serving per a week', \
-    'rockfish': 'Not suggested to eat more than 1 serving per a week', \
+    'white seabass': 'Not recommended to eat more than 1 serving per a week', \
+    'rockfish': 'Not recommended to eat more than 1 serving per a week', \
     'tuna': 'Women who are or may become pregnant, nursing mothers, and young children should NOT eat', \
-    'largemouth bass': 'Not suggested to eat more than 1 serving per a week', \
+    'largemouth bass': 'Not recommended to eat more than 1 serving per a week', \
     'walleye': 'Having 2 servings per week may be OK', \
     'channel catfish': 'Having 2 servings per week may be OK', \
-    'perch': 'Not suggested to eat more than 1 serving per a week', \
-    'carp': 'Not suggested to eat more than 1 serving per a week', \
-    'lake whitefish': 'Not suggested to eat more than 1 serving per a week', \
+    'perch': 'Not recommended to eat more than 1 serving per a week', \
+    'carp': 'Not recommended to eat more than 1 serving per a week', \
+    'lake whitefish': 'Not recommended to eat more than 1 serving per a week', \
     'shad': 'Having 2 servings per week may be OK'
     }
     
     
     file_name = filename
-    model_file = "static/model/retrained_graph.pb"
-    label_file = "static/model/retrained_labels.txt"
+    #model_file = "static/model/retrained_graph.pb"
+    #label_file = "static/model/retrained_labels.txt"
     input_layer = "Placeholder"
     output_layer = "final_result"
                         
@@ -96,10 +62,7 @@ def label_image(filename):
     input_width = 299
     input_mean = 0
     input_std = 255
-                                        
-    graph = load_graph(model_file)
-    labels = load_labels(label_file)
-                                        
+
     t = read_tensor_from_image_file(
         file_name,
         input_height=input_height,
@@ -114,6 +77,8 @@ def label_image(filename):
 
     with tf.Session(graph=graph) as sess:
         results = sess.run(output_operation.outputs[0], {input_operation.outputs[0]: t})
+    sess.close()
+    del sess
     results = np.squeeze(results)
     top_k = results.argsort()[-5:][::-1]
     outputs = []